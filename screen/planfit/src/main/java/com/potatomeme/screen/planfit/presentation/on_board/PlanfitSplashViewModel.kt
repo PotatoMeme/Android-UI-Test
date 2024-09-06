1f9e0a9f42fc16296249a686bfa0937c6837ad35
PotatoMeme <kshk0897@gmail.com>
2024-09-06 21:45:53 +0900

    [:screen:planfit][fix] stateFlow에서 channel로 변경하여 모든변경을 수신할수있도록 변경

--- screen/planfit/src/main/java/com/potatomeme/screen/planfit/presentation/on_board/PlanfitSplashViewModel.kt
+++ screen/planfit/src/main/java/com/potatomeme/screen/planfit/presentation/on_board/PlanfitSplashViewModel.kt
@@ -1,13 +1,19 @@
 package com.potatomeme.screen.planfit.presentation.on_board
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.potatomeme.screen.planfit.data.model.PlanfitLoginType
 import com.potatomeme.screen.planfit.domain.usecase.GetPlanfitLoginType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.runningFold
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,16 +21,21 @@ import javax.inject.Inject
 class PlanfitSplashViewModel @Inject constructor(
     private val getPlanfitLoginType: GetPlanfitLoginType,
 ) : ViewModel() {
-    private val _isLoginFlow = MutableStateFlow<LoginEvent>(LoginEvent.UnChecked)
-    val isLoginFlow: StateFlow<LoginEvent>
-        get() = _isLoginFlow
+    private val _isLoginChanel = Channel<LoginEvent>(Channel.BUFFERED)
+    val isLoginFlow: Flow<LoginEvent> = _isLoginChanel.receiveAsFlow()
 
     fun checkLoginState() = viewModelScope.launch {
-        val loginType = getPlanfitLoginType()
+        _isLoginChanel.send(LoginEvent.UnChecked)
         delay(3000L)
-        _isLoginFlow.emit(LoginEvent.Login(loginType))
+        val loginType = getPlanfitLoginType()
+        Log.d(TAG, "checkLoginState: ${loginType.name}")
+        _isLoginChanel.send(LoginEvent.Login(loginType))
         if (loginType == PlanfitLoginType.None)
-            _isLoginFlow.emit(LoginEvent.UnChecked)
+            _isLoginChanel.send(LoginEvent.UnChecked)
+    }
+
+    companion object {
+        private const val TAG = "PlanfitSplashViewModel"
     }
 }
 
--- screen/planfit/src/main/java/com/potatomeme/screen/planfit/presentation/on_board/SignupSigninFragment.kt
+++ screen/planfit/src/main/java/com/potatomeme/screen/planfit/presentation/on_board/SignupSigninFragment.kt
@@ -1,16 +1,28 @@
 package com.potatomeme.screen.planfit.presentation.on_board
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import com.potatomeme.screen.planfit.data.model.PlanfitLoginType
 import com.potatomeme.screen.planfit.databinding.FragmentSignupSigninBinding
+import com.potatomeme.screen.planfit.domain.usecase.SetPlanfitLoginType
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SignupSigninFragment : Fragment() {
     private lateinit var binding: FragmentSignupSigninBinding
+
+    @Inject
+    lateinit var setPlanfitLoginType: SetPlanfitLoginType
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,5 +34,30 @@ class SignupSigninFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.btnKakaoLogin.setOnClickListener {
+            Log.d(TAG, "onViewCreated: btnKakaoLogin clicked")
+            lifecycleScope.launch {
+                setPlanfitLoginType(PlanfitLoginType.KakaoTalk)
+                view.findNavController().popBackStack()
+            }
+        }
+        binding.btnFacebookLogin.setOnClickListener {
+            Log.d(TAG, "onViewCreated: btnFacebookLogin clicked")
+            lifecycleScope.launch {
+                setPlanfitLoginType(PlanfitLoginType.Facebook)
+                view.findNavController().popBackStack()
+            }
+        }
+        binding.btnGoogleLogin.setOnClickListener {
+            Log.d(TAG, "onViewCreated: btnGoogleLogin clicked")
+            lifecycleScope.launch {
+                view.findNavController().popBackStack()
+                setPlanfitLoginType(PlanfitLoginType.Google)
+            }
+        }
+    }
+
+    companion object{
+        private const val TAG = "SignupSigninFragment"
     }
 }
