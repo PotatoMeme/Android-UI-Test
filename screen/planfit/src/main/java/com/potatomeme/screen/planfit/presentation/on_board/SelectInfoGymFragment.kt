1c588d5eea5dd731be9b01eb15231a93735690a3
PotatoMeme <kshk0897@gmail.com>
2024-09-10 17:38:47 +0900

    [:screen:planfit][feat] SelectInfoGymFragment EditText관련 상호작용 추가

--- screen/planfit/src/main/java/com/potatomeme/screen/planfit/presentation/on_board/SelectInfoGymFragment.kt
+++ screen/planfit/src/main/java/com/potatomeme/screen/planfit/presentation/on_board/SelectInfoGymFragment.kt
@@ -1,23 +1,27 @@
 package com.potatomeme.screen.planfit.presentation.on_board
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.potatomeme.screen.planfit.R
-import com.potatomeme.screen.planfit.databinding.FragmentSelectinfoExerciseLevelBinding
 import com.potatomeme.screen.planfit.databinding.FragmentSelectinfoGymBinding
-import com.potatomeme.screen.planfit.databinding.FragmentSelectinfoPlaceBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class SelectInfoGymFragment : Fragment() {
@@ -60,6 +64,52 @@ class SelectInfoGymFragment : Fragment() {
                 }
             }
         }
+
+        //ivClear 관련
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.ivClear.visibility =
+                    if (p0.isNullOrEmpty()) View.INVISIBLE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+        binding.ivClear.setOnClickListener {
+            binding.etSearch.text.clear()
+        }
+
+
+        //엔터키 상호작용
+        binding.etSearch.setOnKeyListener { v, keyCode: Int, event: KeyEvent ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                viewModel.requestGymList((v as EditText).text.toString())
+                hideKeyboard()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+
+        //d
+        binding.root.setOnTouchListener { _, _ ->
+            if (activity != null && activity?.currentFocus != null) {
+                hideKeyboard()
+            }
+            return@setOnTouchListener false
+        }
+
+
+
+    }
+
+    private fun hideKeyboard() {
+        val inputManager =
+            activity?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(
+            activity?.currentFocus!!.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
 
     }
 }
