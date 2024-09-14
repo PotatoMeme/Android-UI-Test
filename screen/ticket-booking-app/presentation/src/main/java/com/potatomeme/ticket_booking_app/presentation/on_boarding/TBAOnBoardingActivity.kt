16a94c4ab7378ea349ef9aecc4c7d27e78432deb
PotatoMeme <kshk0897@gmail.com>
2024-09-14 17:32:41 +0900

    [feat][#11] TBA OnBoardingActivity 내부 코드 작성

--- screen/ticket-booking-app/presentation/src/main/java/com/potatomeme/ticket_booking_app/presentation/on_boarding/TBAOnBoardingActivity.kt
+++ screen/ticket-booking-app/presentation/src/main/java/com/potatomeme/ticket_booking_app/presentation/on_boarding/TBAOnBoardingActivity.kt
@@ -1,21 +1,27 @@
 package com.potatomeme.ticket_booking_app.presentation.on_boarding
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.potatomeme.ticket_booking_app.presentation.R
+import com.potatomeme.ticket_booking_app.presentation.databinding.ActivityTbaOnBoadingBinding
 
 class TBAOnBoardingActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityTbaOnBoadingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_tba_on_boading)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityTbaOnBoadingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnStart.setOnClickListener {
+            //todo 다음 액티비티로 이동
         }
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
