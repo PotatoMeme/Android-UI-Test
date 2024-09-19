a09595d5025423892f7198bf5dbf9bfb0f5ecea1
PotatoMeme <kshk0897@gmail.com>
2024-09-19 15:27:22 +0900

    [feat][#11] MainActivity ViewPager BannerAdapter 적용, 자동 스크롤 적용

--- screen/ticket-booking-app/presentation/src/main/java/com/potatomeme/ticket_booking_app/presentation/on_boarding/main/TBAMainActivity.kt
+++ screen/ticket-booking-app/presentation/src/main/java/com/potatomeme/ticket_booking_app/presentation/on_boarding/main/TBAMainActivity.kt
@@ -2,6 +2,7 @@ package com.potatomeme.ticket_booking_app.presentation.on_boarding.main
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -10,15 +11,23 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import com.potatomeme.ticket_booking_app.domain.entity.BannerEntity
 import com.potatomeme.ticket_booking_app.presentation.R
 import com.potatomeme.ticket_booking_app.presentation.databinding.ActivityTbaMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.math.abs
 
 @AndroidEntryPoint
 class TBAMainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTbaMainBinding
     private val viewModel: TBAMainViewModel by viewModels()
+    private val bannerAdapter = BannerAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +41,59 @@ class TBAMainActivity : AppCompatActivity() {
                 launch {
                     //확인용
                     viewModel.films.collect {
-                        Log.d(TAG, "onCreate: ${it.size}")
-                        it.forEach {
-                            Log.d(TAG, "onCreate: ${it.casts.size}")
+                        /* Log.d(TAG, "onCreate: ${it.size}")
+                         it.forEach {
+                             Log.d(TAG, "onCreate: ${it.casts.size}")
+                         }*/
+                    }
+                }
+            }
+        }
+        initBanner()
+    }
+
+    private fun initBanner() {
+        viewModel.requestBanners()
+
+        binding.vpBanner.apply {
+            adapter = bannerAdapter
+            clipToPadding = false
+            clipChildren = false
+            offscreenPageLimit = 3
+            (getChildAt(0) as RecyclerView).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+            setPageTransformer(
+                CompositePageTransformer().apply {
+                    addTransformer(MarginPageTransformer(40))
+                    addTransformer { page, position ->
+                        val r = 1 - abs(position)
+                        page.scaleY = 0.85f + r * 0.15f
+                    }
+                }
+            )
+        }
+
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                //viewmodel flow collect
+                launch {
+                    viewModel.banners.collect {
+                        if (it.isEmpty()) {
+                            binding.progressBarSlider.visibility = View.VISIBLE
+                        } else {
+                            binding.progressBarSlider.visibility = View.GONE
+                            bannerAdapter.submitList(it)
+                        }
+                    }
+                }
+                //viewpager auto slide banner
+                launch {
+                    while (true) {
+                        delay(AUTO_SLIDE_DURATION)
+                        val itemCount = binding.vpBanner.adapter?.itemCount ?: 0
+                        if (itemCount > 0) {
+                            val nextItem = (binding.vpBanner.currentItem + 1) % itemCount
+                            binding.vpBanner.setCurrentItem(nextItem, true)
                         }
                     }
                 }
@@ -42,7 +101,9 @@ class TBAMainActivity : AppCompatActivity() {
         }
     }
 
+
     companion object {
         private const val TAG = "TBAMainActivity"
+        private const val AUTO_SLIDE_DURATION = 3000L
     }
 }
