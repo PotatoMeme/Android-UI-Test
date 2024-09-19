266dc257e440605167508e2f9d7afa26dc04c3a1
PotatoMeme <kshk0897@gmail.com>
2024-09-19 15:25:24 +0900

    [feat][#11] requestBanners ViewModel 적용

--- screen/ticket-booking-app/presentation/src/main/java/com/potatomeme/ticket_booking_app/presentation/on_boarding/main/TBAMainViewModel.kt
+++ screen/ticket-booking-app/presentation/src/main/java/com/potatomeme/ticket_booking_app/presentation/on_boarding/main/TBAMainViewModel.kt
@@ -2,7 +2,9 @@ package com.potatomeme.ticket_booking_app.presentation.on_boarding.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.potatomeme.ticket_booking_app.domain.entity.BannerEntity
 import com.potatomeme.ticket_booking_app.domain.entity.FilmEntity
+import com.potatomeme.ticket_booking_app.domain.usecase.RequestBannersUseCase
 import com.potatomeme.ticket_booking_app.domain.usecase.RequestFilmsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,16 +14,25 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TBAMainViewModel @Inject constructor(
-    private val requestFilmsUseCase: RequestFilmsUseCase
+    private val requestFilmsUseCase: RequestFilmsUseCase,
+    private val requestBannersUseCase: RequestBannersUseCase,
 ) : ViewModel() {
 
     private val _films: MutableStateFlow<List<FilmEntity>> = MutableStateFlow(emptyList())
     val films: StateFlow<List<FilmEntity>> = _films
 
+    private val _banners: MutableStateFlow<List<BannerEntity>> = MutableStateFlow(emptyList())
+    val banners: StateFlow<List<BannerEntity>> = _banners
 
+    // request
     fun requestFilms() = viewModelScope.launch() {
         val films = requestFilmsUseCase.invoke()
         _films.value = films
+    }
+
+    fun requestBanners() = viewModelScope.launch() {
+        val banners = requestBannersUseCase.invoke()
+        _banners.value = banners
     }
 
 }
