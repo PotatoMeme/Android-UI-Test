bf4d07c8dc87a2170495489bf4a425a43ac6c54e
PotatoMeme <kshk0897@gmail.com>
2024-09-19 15:17:57 +0900

    [feat][#11] FirebaseSource 에 RequestBanners메서드 추가

--- screen/ticket-booking-app/data/src/main/java/com/potatomeme/ticket_booking_app/data/source/remote/TBAFirebaseSource.kt
+++ screen/ticket-booking-app/data/src/main/java/com/potatomeme/ticket_booking_app/data/source/remote/TBAFirebaseSource.kt
@@ -2,6 +2,7 @@ package com.potatomeme.ticket_booking_app.data.source.remote
 
 import com.google.firebase.database.FirebaseDatabase
 import com.potatomeme.ticket_booking_app.data.model.Film
+import com.potatomeme.ticket_booking_app.data.model.Banner
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
@@ -9,11 +10,10 @@ import javax.inject.Inject
 class TBAFirebaseSource @Inject constructor(
     private val database: FirebaseDatabase,
 ) {
-    private val ref = database.getReference(DATABASE_NAME)
-
-    suspend fun requestFilms(): FirebaseResult {
+    suspend fun requestFilms(): FirebaseResult<Film> {
         return try {
             val items = mutableListOf<Film>()
+            val ref = database.getReference(DATABASE_NAME_ITEMS)
             ref.get().await().children.forEach { snapshot ->
                 snapshot.getValue(Film::class.java)?.let {
                     items.add(it)
@@ -25,13 +25,29 @@ class TBAFirebaseSource @Inject constructor(
         }
     }
 
+    suspend fun requestBanners(): FirebaseResult<Banner> {
+        return try {
+            val items = mutableListOf<Banner>()
+            val ref = database.getReference(DATABASE_NAME_BANNERS)
+            ref.get().await().children.forEach { snapshot ->
+                snapshot.getValue(Banner::class.java)?.let {
+                    items.add(it)
+                }
+            }
+            FirebaseResult.Success(items)
+        } catch (e: Exception) {
+            FirebaseResult.Failure(e)
+        }
+    }
+
     companion object {
         private const val TAG = "TBAFirebaseSource"
-        private const val DATABASE_NAME = "items"
+        private const val DATABASE_NAME_ITEMS = "items"
+        private const val DATABASE_NAME_BANNERS = "banners"
     }
 }
 
-sealed class FirebaseResult {
-    data class Success(val items: List<Film>) : FirebaseResult()
-    data class Failure(val exception: Exception) : FirebaseResult()
+sealed class FirebaseResult<out T> {
+    data class Success<T>(val items: List<T>) : FirebaseResult<T>()
+    data class Failure<T>(val exception: Exception) : FirebaseResult<T>()
 }
