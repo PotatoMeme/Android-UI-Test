2b8ae39e1efadbeafc09ebe895bd56c715afa369
PotatoMeme <kshk0897@gmail.com>
2024-09-20 18:10:08 +0900

    [feat][#11] TBASeatListActivity 생성및 디자인

---
+++ screen/ticket-booking-app/presentation/src/main/java/com/potatomeme/ticket_booking_app/presentation/ui/seat_list/TBASeatListActivity.kt
@@ -0,0 +1,21 @@
+package com.potatomeme.ticket_booking_app.presentation.ui.seat_list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.potatomeme.ticket_booking_app.presentation.R
+
+class TBASeatListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tba_seat_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
