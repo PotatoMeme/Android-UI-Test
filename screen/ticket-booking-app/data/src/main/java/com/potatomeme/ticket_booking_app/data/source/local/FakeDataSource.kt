83e99a6e9015d17a54b549d788761f0ef7a5716a
PotatoMeme <kshk0897@gmail.com>
2024-09-21 11:15:59 +0900

    [feat][#11] FakeDataSource 에 requestTimeSlots & requestDateSlots 추가

--- screen/ticket-booking-app/data/src/main/java/com/potatomeme/ticket_booking_app/data/source/local/FakeDataSource.kt
+++ screen/ticket-booking-app/data/src/main/java/com/potatomeme/ticket_booking_app/data/source/local/FakeDataSource.kt
@@ -1,6 +1,9 @@
 package com.potatomeme.ticket_booking_app.data.source.local
 
 import com.potatomeme.ticket_booking_app.domain.entity.SeatEntity
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 class FakeDataSource @Inject constructor() {
@@ -15,5 +18,26 @@ class FakeDataSource @Inject constructor() {
             seatList.add(SeatEntity(seatStatus, (i + 1).toString()))
         }
         return seatList
+    }
+    suspend fun requestTimeSlots(id: String,date:String): List<String> {
+        val timeSlots = mutableListOf<String>()
+        val formatter = DateTimeFormatter.ofPattern("hh:mm a")
+
+        for (i in 0 until 24 step 2) {
+            val time = LocalTime.of(i, 0)
+            timeSlots.add(time.format(formatter))
+        }
+        return timeSlots
+    }
+
+    suspend fun requestDateSlots(id: String): List<String> {
+        val dates = mutableListOf<String>()
+        val today = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("EEE/dd/MMM")
+
+        for (i in 0 until 7) {
+            dates.add(today.plusDays(i.toLong()).format(formatter))
+        }
+        return dates
     }
 }
