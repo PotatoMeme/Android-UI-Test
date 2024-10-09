0217ccfec2fb5c9a8882d0ef5ec86c6fc0316338
PotatoMeme <kshk0897@gmail.com>
2024-10-09 15:25:34 +0900

    [feat][#21] Compose Basic Layout

---
+++ screen/clone-compose-codelabs/compose-basic-layout/src/main/java/com/potatomeme/screen/clone_compose_codelabs/compose_basic_layout/ui/CBLMainActivity.kt
@@ -0,0 +1,22 @@
+package com.potatomeme.screen.clone_compose_codelabs.compose_basic_layout.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+
+class CBLMainActivity : ComponentActivity() {
+
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val windowSizeClass = calculateWindowSizeClass(this)
+            MySootheApp(windowSizeClass)
+        }
+    }
+}
